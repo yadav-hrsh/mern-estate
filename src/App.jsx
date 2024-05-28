52204637f633d6d62776f8cb8df81057ba6c180e
harsh yadav <princeyadav1692@gmail.com>
2024-05-28 20:58:48 +0530

    create pages and Roues

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 const App = () => {
   return(
-    <div className="bg-red-600">
-        
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 };
 
